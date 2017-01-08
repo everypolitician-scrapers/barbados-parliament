3143e8907eb95ed9a90db5a2547f1f418ff0d145
Tony Bowden <tony@tmtm.com>
2017-01-08 11:43:10 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'date'
 require 'nokogiri'
@@ -11,7 +12,7 @@ require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
 
 def noko_for(url)
-  Nokogiri::HTML(open(url).read) 
+  Nokogiri::HTML(open(url).read)
 end
 
 def date_from(str)
@@ -38,22 +39,22 @@ def scrape_member(url)
 
   party, party_id = party_from(noko.xpath('.//h2[contains(.,"Party")]/following-sibling::p[1]'))
 
-  data = { 
-    id: url[/(\d+)$/, 1],
-    name: noko.css('h1.page-title').text.strip,
-    image: noko.css('div.entry-thumb img/@src').text,
+  data = {
+    id:           url[/(\d+)$/, 1],
+    name:         noko.css('h1.page-title').text.strip,
+    image:        noko.css('div.entry-thumb img/@src').text,
     constituency: noko.css('.post-excerpt').text.strip,
-    party: party,
-    party_id: party_id,
-    role: noko.xpath('.//h3[contains(.,"Designation")]/following-sibling::p').text.strip,
-    telephone: noko.xpath('.//h2[contains(.,"Contact")]/following-sibling::p').text[/Telephone:\s*(.*?)\s*$/, 1],
-    fax: noko.xpath('.//h2[contains(.,"Contact")]/following-sibling::p').text[/Fax:\s*(.*?)\s*$/, 1],
-    email: noko.xpath('.//h2[contains(.,"Contact")]/following-sibling::p').text.lines.find { |l| l.include? '@' }.strip,
-    term: '2013',
-    source: url,
+    party:        party,
+    party_id:     party_id,
+    role:         noko.xpath('.//h3[contains(.,"Designation")]/following-sibling::p').text.strip,
+    telephone:    noko.xpath('.//h2[contains(.,"Contact")]/following-sibling::p').text[/Telephone:\s*(.*?)\s*$/, 1],
+    fax:          noko.xpath('.//h2[contains(.,"Contact")]/following-sibling::p').text[/Fax:\s*(.*?)\s*$/, 1],
+    email:        noko.xpath('.//h2[contains(.,"Contact")]/following-sibling::p').text.lines.find { |l| l.include? '@' }.strip,
+    term:         '2013',
+    source:       url,
   }
   # puts data
-  ScraperWiki.save_sqlite([:id, :term], data)
+  ScraperWiki.save_sqlite(%i(id term), data)
 end
 
 scrape_list('http://www.barbadosparliament.com/member/listall')
